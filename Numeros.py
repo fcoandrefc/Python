d9ec2df908b9f5de570b201dfe59017d2b334c94
Francisco Andre <andrefelixcunha@gmail.com>
2023-06-22 17:50:36 -0300

    Tarefa1

---
+++ Numeros.py
@@ -0,0 +1,31 @@
+n1 = 10;
+n2 = 3;
+
+soma = n1 + n2;
+subritacao = n1-n2;
+mutiplicacao = n1 * n2;
+divisao = n1 / n2;
+resto = n1 % n2;
+potencia = n1 ** n2;
+
+print(soma);
+print(subritacao);
+print(mutiplicacao);
+print(divisao);
+print(round(divisao))
+print(resto);
+print(potencia);
+
+import math;
+n4 = 4.7;
+print(abs(-4.7));
+print(math.ceil(n4));
+print(math.floor(n4));
+
+import random;
+print(random.randint(1,10));
+print(random.random());
+
+n5= 1234.56789;
+print(f"{n5:.2f}");
+print("{:.2f}".format(n5));
---
+++ Tarefa1.py
@@ -0,0 +1,31 @@
+nome = "Fco Andre";
+sobrenome = "Felix Cunha";
+idade = "51";
+frase = "Francisco Andre Felix Cunha";
+nomecompleto = nome +" " + sobrenome;
+n1 = 22;
+n2 = 45;
+soma = n1 + n2;
+
+print(f"Ola, meu nome é {nome} " + f"e eu tenho {idade} anos");
+print();
+print("Tamanho variavel frasse :" + str(len(frase)));
+print();
+print(nomecompleto);
+print();
+print(nomecompleto.upper());
+print();
+print(nomecompleto.split());
+print();
+print(nomecompleto.replace("Francisco","Fco"));
+print();
+print(soma);
+print();
+print("Digite dois numeros inteiros :");
+s1 = int(input());
+s2 = int(input());
+print("Mutiplicação dos Inteiros: " + str(s1 * s2));
+ 
+
+
+
