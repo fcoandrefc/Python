70f593fddbb9aa4d3d8dbfb7826ad83e59fc4720
Francisco Andre <andrefelixcunha@gmail.com>
2023-06-29 19:46:56 -0300

    Operadores Aritmeticos

---
+++ Estruturas-operadores/Operadoresaritmeticos.py
@@ -0,0 +1,18 @@
+a = 10;
+b = 3;
+
+soma             = a - b;
+subtracao        = a - b;
+mutiplicacao     = a * b;
+divisao          = a / b;
+resto            = a % b;
+potencia         = a ** b;
+restodescartado  =  a // b;
+
+print("Soma :", soma);
+print("Subtração: ", subtracao);
+print("Mutiplicação: ",mutiplicacao);
+print("Divisão: ", divisao);
+print("Resto Divisão: ",resto);
+print("Potencia: ",potencia);
+print("Resto Descartado: ",restodescartado);
