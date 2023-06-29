3e2d54053791850f4095bc99a60f3dcaa2e9391e
Francisco Andre <andrefelixcunha@gmail.com>
2023-06-28 21:58:05 -0300

    Exercicios

---
+++ Exercicios/Tarefa2.py
@@ -0,0 +1,16 @@
+Frutas =  ["maca","banana", "laranja", "abacaxi", "melancia"];
+
+Frutas.append("uva");
+print(Frutas);
+Frutas.remove("uva");
+print(Frutas);
+
+fruta_selecionada = Frutas[1];
+print(fruta_selecionada);
+
+
+Cores = ("vermelho", "azul", "verde", "amarelo", "roxo");
+print(Cores);
+cor_selecionada = Cores[2];
+print(cor_selecionada);
+
