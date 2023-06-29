4ae22fb74de756a9cbe13747eec9037ed06758fa
Francisco Andre <andrefelixcunha@gmail.com>
2023-06-29 17:22:58 -0300

    Exercicios

--- Exercicios/Tarefa2.py
+++ Exercicios/Tarefa2.py
@@ -14,3 +14,27 @@ print(Cores);
 cor_selecionada = Cores[2];
 print(cor_selecionada);
 
+# tupla e imutavel, não aceita o metodo append.
+# Cores.append("laranja");
+
+aluno={"nome": "André",
+        "idade": 51,
+        "cidade": "Fortaleza"};
+
+print(aluno);
+idade_aluno = aluno["idade"];
+print(idade_aluno);
+
+aluno["genero"]="masculio";
+print(aluno);
+
+aluno.pop("cidade");
+print(aluno);
+
+
+
+      
+
+
+
+
