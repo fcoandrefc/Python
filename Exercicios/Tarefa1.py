2cb6bef7b96a9f9cce98213b4ed309650e941638
Francisco Andre <andrefelixcunha@gmail.com>
2023-06-28 21:49:46 -0300

    Exercicios

---
+++ Exercicios/Tarefa1.py
@@ -0,0 +1,31 @@
+nome = "Fco Andre";
+sobrenome = "Felix Cunha";
+idade = "51";
+frase = "Uma vez Flamengo Sempre Flamengo";
+nomecompleto = nome +" " + sobrenome;
+n1 = 22;
+n2 = 45;
+soma = n1 + n2;
+
+print(f"Ola, meu nome é {nome} e eu tenho {idade} anos");
+print();
+print("Tamanho variavel frasse :", len(frase));
+print();
+print(nomecompleto);
+print();
+print(frase.upper());
+print();
+print(frase.split());
+print();
+print(frase.replace("Flamengo","MENGO"));
+print();
+print(soma);
+print();
+print("Digite dois numeros inteiros :");
+s1 = int(input());
+s2 = int(input());
+print("Mutiplicação dos Inteiros: " + str(s1 * s2));
+ 
+
+
+
