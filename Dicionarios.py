fbf7d32131fd2027698d33adda7f9e37490fe330
Francisco Andre <andrefelixcunha@gmail.com>
2023-06-28 21:36:08 -0300

    python

--- Dicionarios.py
+++ Dicionarios.py
@@ -1,4 +1,35 @@
-meuDicioanrio = {"nome":"Andre",
-                 "sobrenome": "Felix"};
-
+meuDicioanrio = {"nome"        :"Andre",
+                 "sobrenome"   : "Felix",
+                 "anos"        : 51
+                 }
 print(meuDicioanrio);
+
+frutaDicioanrio = {
+    "maca" : 3 ,
+    "banana" : 4,
+    "uva" : 5       
+}
+
+frutaDicioanrio["maca"] = 8;
+
+print(frutaDicioanrio["maca"]);
+
+animaisDicioanrio = {};
+
+aluno = {
+    "nome"    : "Andre",
+    "idade"   : 51,
+    "hobbies" : ['jogar', 'comer']
+}
+print(aluno)
+print(aluno["hobbies"][1]);
+
+print(aluno.get("nome"));
+print(aluno.get("nomes", "erro"));
+
+aluno.pop("idade");
+print(aluno);
+
+print(aluno.keys());
+print(aluno.values());
+print(aluno.items());
