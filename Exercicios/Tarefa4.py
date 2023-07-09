d7acb977fabb15beab3c8d4a689d484beeab8800
Francisco Andre <andrefelixcunha@gmail.com>
2023-07-09 20:25:58 -0300

    Tarefas

---
+++ Exercicios/Tarefa4.py
@@ -0,0 +1,87 @@
+# 1
+n1 = int(input("Digite Primento numero inteiro: "));
+n2 = int(input("Digite Segundo numero inteiro: "));
+
+print(n1 + n2);
+print(n1 - n2);
+print(n1 * n2);
+print(n1 / n2);
+
+#2
+temperatura = int(input("Digite a Temperatura graus Celsius: "));
+if temperatura > 100:
+  print("A agua está fervendo!");
+
+#3
+n3 = int(input("Digite numero inteiro: "));
+if (n3 % 2 == 0):
+  print("Numero Par")
+else:
+  print("Numero Impar");
+
+#4
+senha = input("Digite senha: ");
+if senha == "123456":
+  print("Senha Correta")
+else:
+  print("Senha Errada");
+
+#5
+idade = int(input("Digite sua Idade: "));
+if 18 <= idade <=30:
+  print("Esta entre 18 e 30")
+else:
+  print("Nao esta em 18 e 30");
+
+#6  
+n4 = int(input("Digite 1 numero inteiro: "));
+n5 = int(input("Digite 2 numero inteiro: "));
+n6 = int(input("Digite 3 numero inteiro: "));
+
+if n4 >= 0 or n5 >= 0 or n6 >= 0:
+    print ("Tem numero positivo")
+else:
+    print("Não tem numero positivo");
+
+#7    
+letra = input("Digite uma letra:");  
+Listavogais =["a","e","i","o","u"];
+if letra in Listavogais:
+   print("Letra e um vogal!")
+else:
+    print("Letra não e uma vogal") ;
+
+#8    
+n7 = int(input("Digite numero inteiro: "));
+if n7 > 0:
+  print("Numero Positivo")
+elif n7<0:
+  print("Numero negativo")
+else:
+   print("Numero zero");
+
+#9
+n8 = int(input("Digite 1 numero inteiro: "));
+n9 = int(input("Digite 2 numero inteiro: "));
+n10 = int(input("Digite 3 numero inteiro: "));
+
+if n9 < n8 or n10 < n8:
+  print("Numero fora da order crescente")
+else:
+   print("Numero na ordem  crescente");
+
+#10
+n11 = int(input("Digite numero inteiro: "));
+
+if (n11 % 3 == 0) and (n11 % 5 == 0) :
+  print("Numero mutiplo de 3 e 5")
+else:
+  print("Numero nao é mutiplo de 3 e 5");
+
+
+
+
+
+    
+
+  
--- Lacos_repeticao/For.py
+++ Lacos_repeticao/For.py
@@ -1,5 +1,10 @@
-for numero in range(1,11):
-    print(numero)
-    
+letra = "python"
+Listavogais =["a","e","i","o","u"];
+vogal = 0
 
+for lista in Listavogais:
+ if lista in letra:
+   vogal += 1
+ 
+print(vogal) ;
 
